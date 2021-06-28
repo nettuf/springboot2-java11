e44c489d79c0719d4fc296c84088c030b23a2760
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-06-28 16:46:17 -0300

    User entity and resource

---
+++ src/main/java/com/nettuf/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.nettuf.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nettuf.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "Maria@gmail.com", "999965", "23121");
+		return ResponseEntity.ok().body(u);
+	}
+}
