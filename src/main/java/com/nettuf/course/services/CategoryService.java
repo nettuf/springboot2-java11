b84c01a83be965dd4ebd52327a7d8699ea179b3f
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-06-29 16:48:05 -0300

    category implementation

---
+++ src/main/java/com/nettuf/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.nettuf.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nettuf.course.entities.Category;
+import com.nettuf.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
