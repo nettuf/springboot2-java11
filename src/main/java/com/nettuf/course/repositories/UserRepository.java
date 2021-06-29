6db88e554103295ad360394eacf988b61fdd7673
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-06-29 12:23:16 -0300

    service layer, component registration

--- src/main/java/com/nettuf/course/repositories/UserRepository.java
+++ src/main/java/com/nettuf/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.nettuf.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.nettuf.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
