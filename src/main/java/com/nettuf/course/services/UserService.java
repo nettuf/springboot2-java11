1f1513366b5e2dd8d5d424b1f38cffce94ebc13f
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-07-05 12:46:18 -0300

    exception handling - delete

--- src/main/java/com/nettuf/course/services/UserService.java
+++ src/main/java/com/nettuf/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.nettuf.course.entities.User;
 import com.nettuf.course.repositories.UserRepository;
+import com.nettuf.course.services.exceptions.DatabaseException;
 import com.nettuf.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +34,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update(Long id, User obj) {
